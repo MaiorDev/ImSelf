5c392ad294ebc866e0f47f03a7a0948182060312
Maior <marianojeronimobautista@gmail.com>
2025-03-15 15:18:33 -0600

    1.1.0 HabitTracker working

--- src/app.js
+++ src/app.js
@@ -12,6 +12,22 @@ app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
+app.get("/projects", (req, res) => {
+  res.render("projects.ejs");
+});
+
+app.get("/school", (req, res) => {
+  res.render("school.ejs");
+});
+
+app.get("/objectives", (req, res) => {
+  res.render("objectives.ejs");
+});
+
+app.get("/notes", (req, res) => {
+  res.render("notes.ejs");
+});
+
 app.get("/habitTracker", (req, res) => {
   const consult = "SELECT * FROM habits";
   connection.query(consult, (error, results) => {
@@ -22,17 +38,61 @@ app.get("/habitTracker", (req, res) => {
   });
 });
 
-app.post("/addHabit", (req, res) => {
+app.post("/habitTracker/addHabit", (req, res) => {
   const data = req.body;
   const title = data.title;
   const description = data.description;
-  const consult = `INSERT INTO habits (title, description, status) VALUES (?, ?, 'pending')`;
-  connection.query(consult, [title, description], (err) => {
+  const date = new Date();
+  const consult = `INSERT INTO habits (title, description, status, date) VALUES (?, ?, 'pending', ?)`;
+  connection.query(consult, [title, description, date], (err) => {
     if (err) {
       console.error("Error consult", err.message);
       return res.status(500).send("Error in the registration of the habit");
     }
     res.redirect("/habitTracker");
+  });
+});
+
+// Update habit Status
+app.patch("/habitTracker/update-habit-status/:id", (req, res) => {
+  const id = req.params.id;
+  const status = req.body.status;
+  const consult = "UPDATE habits SET  status = ?  WHERE id_habit = ?";
+  connection.query(consult, [status, id], (err) => {
+    if (err) {
+      console.error("Error consult", err.message);
+      return res.status(500).send("Error updating the habit");
+    }
+    res.json();
+  });
+});
+
+//update habit data
+app.patch("/habitTracker/update-habit-data/:id", (req, res) => {
+  const id = req.params.id;
+  const title = req.body.title;
+  const description = req.body.description;
+  const consult =
+    "UPDATE habits SET title = ?, description = ?  WHERE id_habit = ?";
+  connection.query(consult, [title, description, id], (err) => {
+    if (err) {
+      console.error("Error consult", err.message);
+      return res.status(500).send("Error updating the habit");
+    }
+    res.json();
+  });
+});
+
+// Delete habit
+app.delete("/habitTracker/delete-habit/:id", (req, res) => {
+  const id = req.params.id;
+  const consult = "DELETE FROM habits WHERE id_habit = ?";
+  connection.query(consult, [id], (err) => {
+    if (err) {
+      console.error("Error consult", err.message);
+      return res.status(500).send("Error deleting the habit");
+    }
+    res.json();
   });
 });
 const PORT = 3000;
--- src/public/js/habitManager.js
+++ src/public/js/habitManager.js
@@ -1,27 +1,87 @@
 //Move habits
+let draggedHabit = null;
 
-const habits = document.getElementsByClassName("habit");
-
-for (let habit of habits) {
-  // Añadir el evento de arrastre a cada elemento
-  habit.addEventListener("dragstart", (e) => {
-    // Verifica si el atributo id_habit existe en el elemento
-
-    // Poner el id del hábito en el objeto de transferencia
-    e.dataTransfer.setData("id", habit.id_habit);
-    console.log("ID transferido:", habit.id_habit);
+// make the habits draggable
+const habits = document.querySelectorAll(".habit");
+habits.forEach((habit) => {
+  habit.addEventListener("dragstart", (event) => {
+    draggedHabit = event.target; // save the habit being dragged
+    event.target.classList.add("dragging"); // add a class to indicate the habit is being dragged
   });
+
+  habit.addEventListener("dragend", () => {
+    draggedHabit.classList.remove("dragging");
+    draggedHabit.style.opacity = "1"; // Restore opacity
+    draggedHabit = null;
+  });
+});
+
+// make the areas droppable
+document.querySelectorAll(".droppable").forEach((area) => {
+  area.addEventListener("dragover", (event) => {
+    event.preventDefault();
+    event.target.classList.add("drag-over");
+  });
+  area.addEventListener("dragleave", () => {
+    event.target.classList.remove("drag-over");
+  });
+  area.addEventListener("drop", (event) => {
+    event.preventDefault();
+    event.target.classList.remove("drag-over");
+    if (draggedHabit) {
+      area.appendChild(draggedHabit);
+      updateHabitStatus(draggedHabit.dataset.id, area.dataset.status);
+      console.log(draggedHabit.dataset.id);
+    }
+  });
+});
+
+// Update the status of the habit
+function updateHabitStatus(habitId, newStatus) {
+  fetch(`/habitTracker/update-habit-status/${habitId}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ status: newStatus }),
+  })
+    .then(() => {
+      console.log("Habit updated");
+    })
+    .catch((error) => console.error("Error updating habit:", error));
 }
 
 //Add Habit with the form
 const addHabit = document.getElementById("add-Habit");
 const form = document.getElementById("container-form");
-
+const titleInput = document.getElementById("titleInput");
+const submitAddHabit = document.getElementById("submitAddHabit");
+//open the form
 addHabit.addEventListener("click", () => {
   form.style.display = "flex";
 });
 
+titleInput.addEventListener("input", (e) => {
+  let titleValue = e.target.value;
+  if (titleValue.length == 0) {
+    submitAddHabit.disabled = true;
+    submitAddHabit.style.opacity = "0.5";
+    submitAddHabit.style.backgroundColor = "#666";
+    submitAddHabit.style.cursor = "not-allowed";
+  } else {
+    submitAddHabit.disabled = false;
+    submitAddHabit.style.opacity = "1";
+    submitAddHabit.style.backgroundColor = "#0d7d69";
+    submitAddHabit.style.cursor = "pointer";
+  }
+
+  if (titleValue.length > 25) {
+    titleValue = titleValue.substring(0, 25);
+    titleInput.value = titleValue;
+  }
+});
 document.getElementById("newHabitForm").addEventListener("submit", (e) => {
+  e.preventDefault();
   const titleInput = document.getElementById("titleInput").value.trim();
   const descriptionInput = document
     .getElementById("descriptionInput")
@@ -32,7 +92,7 @@ document.getElementById("newHabitForm").addEventListener("submit", (e) => {
     return;
   }
 
-  fetch("/addHabit", {
+  fetch("/habitTracker/addHabit", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
@@ -41,9 +101,91 @@ document.getElementById("newHabitForm").addEventListener("submit", (e) => {
     }),
   })
     .then(() => {
-      location.reload();
+      window.location.reload();
     })
     .catch((error) => {
       console.error("Error en la solicitud:", error);
+      window.location.reload();
     });
+});
+
+//close the form
+document.getElementById("close-form").addEventListener("click", (e) => {
+  e.stopPropagation();
+  e.preventDefault();
+  form.style.display = "none";
+});
+
+//interact with the habit
+const infocard = document.getElementById("container-info-card");
+const title = document.getElementById("title_info-card");
+const description = document.getElementById("description_info-card");
+const closeInfoCard = document.getElementById("close-info-card");
+const edit = document.getElementById("edit-habit");
+
+closeInfoCard.addEventListener("click", () => {
+  infocard.style.display = "none";
+});
+
+habits.forEach((habit) => {
+  habit.addEventListener("click", (e) => {
+    infocard.style.display = "flex";
+    title.value = habit.dataset.title;
+    description.value = habit.dataset.description;
+    infocard.dataset.id = habit.dataset.id;
+  });
+});
+
+//edit habit button
+title.addEventListener("input", (e) => {
+  let titleValue = e.target.value;
+  if (titleValue.length == 0) {
+    edit.style.display = "none";
+  } else {
+    edit.style.display = "block";
+  }
+
+  if (titleValue.length > 25) {
+    titleValue = titleValue.substring(0, 25);
+    title.value = titleValue;
+  }
+});
+
+title.addEventListener("keypress", (e) => {
+  edit.style.display = "block";
+});
+description.addEventListener("keypress", (e) => {
+  edit.style.display = "block";
+});
+// Edit habit
+edit.addEventListener("click", () => {
+  const newDescription = description.value;
+  const newTitle = title.value;
+  const id = infocard.dataset.id;
+
+  fetch(`/habitTracker/update-habit-data/${id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ description: newDescription, title: newTitle }),
+  })
+    .then(() => {
+      console.log("Description updated");
+      window.location.reload();
+    })
+    .catch((error) => console.error("Error updating description:", error));
+});
+//delete habit
+const deleteHabit = document.getElementById("delete-habit");
+deleteHabit.addEventListener("click", () => {
+  const id = infocard.dataset.id;
+  fetch(`/habitTracker/delete-habit/${id}`, {
+    method: "DELETE",
+  })
+    .then(() => {
+      console.log("Habit deleted");
+      window.location.reload();
+    })
+    .catch((error) => console.error("Error deleting habit:", error));
 });
